1526664393967cb432706835e3a7465a926b1d50
Victor Popescu <victor@emotionconcept.ro>
2015-10-17 11:22:16 +0300

    Session (5) update

--- Session (5)/Network Traffic Extension/application/extension.js
+++ Session (5)/Network Traffic Extension/application/extension.js
@@ -1,7 +1,19 @@
 	
 	"use strict";
 
-	chrome.devtools.network.onRequestFinished.addListener( function( request )
+	chrome.tabs.onCreated.addListener(function( tabObject )
 	{
-		console.log( request );
+		console.log( "--Created--", tabObject );
+	});
+
+	chrome.tabs.onUpdated.addListener(function( tabID, changeInfo, tabObject )
+	{
+		console.log( "--Updated--", tabID, changeInfo, tabObject );
+
+		//chrome.tabs.create( { url: "home.ro", active: true } );
+	});
+
+	chrome.tabs.onRemoved.addListener(function( tabID, removeInfo )
+	{
+		console.log( "--Removed--", tabID, removeInfo );
 	});
