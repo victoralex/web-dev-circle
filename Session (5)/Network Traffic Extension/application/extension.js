7d56fbd3dc6531901a7d299d6603c5051e770095
Victor Popescu <victor@emotionconcept.ro>
2015-10-17 12:16:07 +0300

    Session (3) update

--- Session (5)/Network Traffic Extension/application/extension.js
+++ Session (5)/Network Traffic Extension/application/extension.js
@@ -1,32 +1,15 @@
 	
 	"use strict";
 
-	chrome.tabs.onCreated.addListener(function( tabObject )
-	{
-		console.log( "--Created--", tabObject );
-	});
+	var	trackerIP = '192.168.105.65',
+		peer = new Peer(
+			'victor',
+			{
+				host: trackerIP
+			}
+		);
 
 	chrome.tabs.onUpdated.addListener(function( tabID, changeInfo, tabObject )
 	{
 		console.log( "--Updated--", tabID, changeInfo, tabObject );
-
-		//chrome.tabs.create( { url: "home.ro", active: true } );
 	});
-
-	chrome.tabs.onRemoved.addListener(function( tabID, removeInfo )
-	{
-		console.log( "--Removed--", tabID, removeInfo );
-	});
-
-	chrome.runtime.onMessage.addListener( function(request, sender, sendResponse)
-	{
-    	console.log(
-    		sender.tab ? "from a content script:" + sender.tab.url : "from the extension",
-    		request
-    	);
-
-    	if( request.greeting == "hello" )
-    	{
-      		sendResponse({farewell: "goodbye"});	
-    	}
-  });
